789377a778c64fffe61926232b36d7f0cc7a2fbc
Nathan Lowrie <nate@finelineautomation.com>
2013-10-01 12:55:30 -0400

    Got the blogs index page and menuing working
    Got the menu for blogs and disqus settings working. Got the Disqus settings styled. Got the blog index styled.
    
    Signed-off-by: Nathan Lowrie <nate@finelineautomation.com>

--- app/overrides/spree_essential_blog.rb
+++ app/overrides/spree_essential_blog.rb
@@ -1,5 +1,4 @@
-Deface::Override.new(:virtual_path  => "spree/admin/configurations/index",
-                     :name          => "blog_disqus_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :partial       => "spree/blogs/admin/shared/blog_config",
-                     :disabled      => false)
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+                     :name => "add_active_shipping_settings_tab",
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
+                     :partial       => "spree/admin/shared/blog_config")
